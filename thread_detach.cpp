b49ec9046f183e19a55c97d08635cba8913bfe0f
choisungjune <chltjdwns88@gmail.com>
2022-01-02 23:32:09 +0900

    thread_detach_background

---
+++ thread_detach.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+void func1(){
+    for(int i = 0; i < 10; i++)
+        std::cout << "쓰레드 1 실행중" << '\n';
+}
+
+void func2(){
+    for(int i = 0; i < 10; i++)
+        std::cout << "쓰레드 2 실행중" << '\n';
+}
+
+void func3(){
+    for(int i = 0; i < 10; i++)
+        std::cout << "쓰레드 3 실행중" << '\n';
+}
+
+int main()
+{
+    std::thread t1(func1);
+    std::thread t2(func2);
+    std::thread t3(func3);
+
+    t1.detach();
+    t2.detach();
+    t3.detach();   //백그라운드로 실행
+    std::cout << "Main 함수 종료" << '\n';
+    return 0;
+}
